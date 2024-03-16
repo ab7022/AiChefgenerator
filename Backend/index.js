4903b26273dc8c354d8bf6ab106324dd53e47f97
Abdul Bayees <abdul@arch.localdomain>
2024-03-16 22:39:58 +0530

    added vercel.json

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const { User } = require('./database')
-const  router = express.Router()
-const AllRoutes = require("./AllRoutes")
-const app = express()
-const cors = require("cors")
-app.use(cors({
-    origin: 'https://ai-chefgenerator-zw59.vercel.app',
-    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));app.use(express.json())
-app.use(AllRoutes)
+const express = require("express");
+const { User } = require("./database");
+const router = express.Router();
+const AllRoutes = require("./AllRoutes");
+const app = express();
+const cors = require("cors");
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://ai-chefgenerator-zw59.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+app.use(express.json());
+app.use(AllRoutes);
 
-
-app.listen(3000)
+app.listen(3000);
